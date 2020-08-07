7036c06d46ab5be98ec6fab9b61bb91c72861608
Don1k1337 <dan4ik1411@gmail.com>
2020-08-07 14:55:32 +0600

    Created User registration in views.py, and created corresponded HTML templates for registration form.

--- account/views.py
+++ account/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
 from django.contrib.auth.decorators import login_required
+
 
 def user_login(request):
     if request.method == 'POST':
@@ -25,8 +26,33 @@ def user_login(request):
         form = LoginForm()
     return render(request, 'account/login.html', {'form': form})
 
+
 @login_required
 def dashboard(request):
     return render(request,
                   'account/dashboard.html',
                   {'section': 'dashboard'})
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # Create a new user object but avoid saving it yet
+            new_user = user_form.save(commit=False)
+            '''Sets the user’s password to the given raw string, taking care of the password hashing. Doesn't save 
+            the User object. When the raw_password is None, the password will be set to an unusable password, 
+            as if set_unusable_password() were used. '''
+            # Set chosen password
+            new_user.set_password(
+                user_form.cleaned_data['password'])
+            # Save the User object
+            new_user.save()
+            return render(request,
+                          'account/register_done.html',
+                          {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request,
+                  'account/register.html',
+                  {'user_form': user_form})
