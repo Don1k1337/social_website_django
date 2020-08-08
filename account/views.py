f110169c2f27035013c770071a652242ed3033fa
Don1k1337 <dan4ik1411@gmail.com>
2020-08-08 16:16:06 +0600

    Modified .views.py for error messages showing with the help of django messages framework.

--- account/views.py
+++ account/views.py
@@ -5,6 +5,7 @@ from .forms import LoginForm, UserRegistrationForm, \
     UserEditForm, ProfileEditForm
 from django.contrib.auth.decorators import login_required
 from .models import Profile
+from django.contrib import messages
 
 
 def user_login(request):
@@ -64,20 +65,24 @@ def register(request):
 
 @login_required
 # view func to edit existing profiles
+@login_required
 def edit(request):
-    # Therefore a better way to get the "profile" from model User
-    profile = Profile(user=request.user)
     if request.method == 'POST':
-        user_form = UserEditForm(instance=request.user, data=request.POST)
-        profile_form = ProfileEditForm(instance=profile,
-                                       data=request.POST,
-                                       files=request.FILES)
+        user_form = UserEditForm(instance=request.user,
+                                 data=request.POST)
+        profile_form = ProfileEditForm(
+                                    instance=request.user.profile,
+                                    data=request.POST,
+                                    files=request.FILES)
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, 'Profile updated successfully')
+        else:
+            messages.error(request, 'Error updating your profile, please try again')
     else:
         user_form = UserEditForm(instance=request.user)
-        profile_form = ProfileEditForm(instance=profile)
+        profile_form = ProfileEditForm(instance=request.user.profile)
     return render(request,
                   'account/edit.html',
                   {'user_form': user_form,
