eec3aa98ebc52acac4c3bcc6b77833e536fd9234
Don1k1337 <dan4ik1411@gmail.com>
2020-08-16 14:20:15 +0600

    Modified detail.html, added user_follow view inside views.py for following action between existing users.

--- account/views.py
+++ account/views.py
@@ -1,12 +1,14 @@
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
+from django.views.decorators.http import require_POST
 from .forms import LoginForm, UserRegistrationForm, \
     UserEditForm, ProfileEditForm
 from django.contrib.auth.decorators import login_required
-from .models import Profile
+from common.decorators import ajax_required
+from .models import Profile, Contact
 from django.contrib import messages
 
 
@@ -93,7 +95,7 @@ def edit(request):
 @login_required
 def user_list(request):
     users = User.objects.filter(is_active=True)
-    paginator = Paginator(users, 2)
+    paginator = Paginator(users, 5)
     page = request.GET.get('page')
     try:
         users = paginator.page(page)
@@ -119,3 +121,24 @@ def user_detail(request, username):
                   'account/user/detail.html',
                   {'section': 'people',
                    'user': user})
+
+
+@ajax_required
+@require_POST
+@login_required
+def user_follow(request):
+    user_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if user_id and action:
+        try:
+            user = User.objects.get(id=user_id)
+            if action == 'follow':
+                Contact.objects.get_or_create(user_from=request.user,
+                                              user_to=user)
+            else:
+                Contact.objects.filter(user_from=request.user,
+                                       user_to=user).delete()
+            return JsonResponse({'status': 'ok'})
+        except User.DoesNotExist:
+            return JsonResponse({'status': 'error'})
+    return JsonResponse({'status': 'error'})
