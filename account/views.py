f31840fd053f0c6786c731ae6f2e52e378a00b91
Don1k1337 <dan4ik1411@gmail.com>
2020-08-17 20:58:30 +0600

    Modified views.py for actions by utils.py func create_action, added for an appropriate fields in view.

--- account/views.py
+++ account/views.py
@@ -10,6 +10,8 @@ from django.contrib.auth.decorators import login_required
 from common.decorators import ajax_required
 from .models import Profile, Contact
 from django.contrib import messages
+from actions.utils import create_action
+from actions.models import Action
 
 
 def user_login(request):
@@ -36,9 +38,20 @@ def user_login(request):
 
 @login_required
 def dashboard(request):
+    # Display all actions by default
+    actions = Action.objects.exclude(user=request.user)
+    # flat=True arg - retrieve records like single value,
+    # not a single element tuple (Available only for 1 field)
+    following_ids = request.user.following.values_list('id',
+                                                       flat=True)
+    if following_ids:
+        # If user is following others, retrieve only their actions
+        actions = actions.filter(user_id__in=following_ids)
+    actions = actions[:10]
     return render(request,
                   'account/dashboard.html',
-                  {'section': 'dashboard'})
+                  {'section': 'dashboard',
+                   'actions': actions})
 
 
 def register(request):
@@ -57,6 +70,7 @@ def register(request):
             new_user.save()
             # Creating the user profile
             Profile.objects.create(user=new_user)
+            create_action(new_user, 'has created an account')
             return render(request,
                           'account/register_done.html',
                           {'new_user': new_user})
@@ -135,6 +149,7 @@ def user_follow(request):
             if action == 'follow':
                 Contact.objects.get_or_create(user_from=request.user,
                                               user_to=user)
+                create_action(request.user, 'is following', user)
             else:
                 Contact.objects.filter(user_from=request.user,
                                        user_to=user).delete()
--- images/views.py
+++ images/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from actions.utils import create_action
 from .forms import ImageCreateForm
 from .models import Image
 from django.shortcuts import get_object_or_404
@@ -21,10 +22,10 @@ def image_create(request):
             # form data is valid
             cd = form.cleaned_data
             new_item = form.save(commit=False)
-
             # assign current user to the item
             new_item.user = request.user
             new_item.save()
+            create_action(request.user, 'bookmarked image', new_item)
             messages.success(request, 'Image added successfully')
 
             # redirect to new created item detail view
@@ -58,6 +59,7 @@ def image_like(request):
             image = Image.objects.get(id=image_id)
             if action == 'like':
                 image.users_like.add(request.user)
+                create_action(request.user, 'likes', image)
             else:
                 image.users_like.remove(request.user)
             return JsonResponse({'status': 'ok'})
